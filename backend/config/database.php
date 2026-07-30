ee0f6905db7fc2d104d35baa07486b4d1b95f459
hernan-florez-admin <herda092004@gmail.com>
2026-07-30 01:49:54 -0500

    fix(db): enable SSL TLS connection options for TiDB Cloud Serverless

--- backend/config/database.php
+++ backend/config/database.php
@@ -60,8 +60,11 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') ?: (file_exists('/etc/ssl/certs/ca-certificates.crt') ? '/etc/ssl/certs/ca-certificates.crt' : null),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', false),
+            ], function ($value) {
+                return !is_null($value) && $value !== '';
+            }) : [],
         ],
 
         'mariadb' => [
